956ab19519d64c676a7e471934c7dc005c1b7b87
YogitaDevi <yogita.gry@gmail.com>
2022-12-20 00:00:31 +0530

    Checking Mobile Number

---
+++ UC4_MobileNumber/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC4_MobileNumber
+{
+    public class Pattern
+    {
+        public static string MobileNumber = @"^\+?\d{0,2}\-?\d{4,5}\-?\d{5,6}";
+        public bool validate_Number(string Number)
+        {
+            return Regex.IsMatch(Number, MobileNumber);
+        }
+    }
+}
---
+++ UC4_MobileNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC4_MobileNumber;
+
+namespace UC4_MobileNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter the Mobile Number: ");
+            string Number = Console.ReadLine();
+            bool number = p.validate_Number(Number);
+            if (number)
+            {
+                Console.WriteLine("valid mobile number.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid mobile number");
+            }
+        }
+    }
+}
