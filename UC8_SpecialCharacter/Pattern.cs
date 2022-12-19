15d88edbc2e1d8dddb7c07834aaf2cbc9aef83a9
YogitaDevi <yogita.gry@gmail.com>
2022-12-20 00:47:46 +0530

    [yogita] 1 Special Character

---
+++ UC8_SpecialCharacter/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC8_SpecialCharacter
+{
+    public class Pattern
+    {
+        public static string Password = @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]";
+        public bool validate_pwd(string password)
+        {
+            return Regex.IsMatch(password, Password);
+        }
+    }
+}
---
+++ UC8_SpecialCharacter/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC8_SpecialCharacter;
+
+namespace UC8_SpecialCharacter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+            bool pwd = p.validate_pwd(password);
+            if (pwd)
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
