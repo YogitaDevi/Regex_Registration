1d3c1fcdca9baed5058636a15c60866e8e757736
YogitaDevi <yogita.gry@gmail.com>
2022-12-20 00:29:50 +0530

    [yogita]at least 1 upper case

---
+++ UC6_UpperCase/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC6_UpperCase
+{
+    internal class Pattern
+    {
+        public static string Password = @"[A-Z]+";
+        public bool validate_pwd(string password)
+        {
+            return Regex.IsMatch(password, Password);
+        }
+    }
+}
---
+++ UC6_UpperCase/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC6_UpperCase;
+
+namespace UC6_UpperCase
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+            bool pwd = p.validate_pwd(password);
+            if (pwd)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
