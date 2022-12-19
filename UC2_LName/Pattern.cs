ba5789cf400513bf860dd7cf2fd1c83ff7ac9a91
YogitaDevi <yogita.gry@gmail.com>
2022-12-19 22:24:06 +0530

    Checking Last name

---
+++ UC2_LName/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC2_LName
+{
+    public class Pattern
+    {
+        public static string LName = "^([A-Z]+?)([a-zA-Z]+)*?$";
+        public bool validate_lname(string lname)
+        {
+            return Regex.IsMatch(lname, LName);
+        }
+    }
+}
---
+++ UC2_LName/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC2_LName;
+
+namespace UC2_LName
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter the Last Name: ");
+            string Lname = Console.ReadLine();
+            bool lname = p.validate_lname(Lname);
+            if (lname)
+            {
+                Console.WriteLine("Valid last name");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
