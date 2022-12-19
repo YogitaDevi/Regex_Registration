a7576d6aa27bae3cec485fe829522507c237b579
YogitaDevi <yogita.gry@gmail.com>
2022-12-19 23:28:04 +0530

    Checking Email

---
+++ UC3_Email/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC3_Email
+{
+    public class Pattern
+    {
+        public static string Email = @"^[a-zA-Z0-9\._-|+]{3,25}.@.[a-z]{0,12}.(com|org|co\.in|net)";
+        public bool validate_email(string email)
+        {
+            return Regex.IsMatch(email, Email);
+        }
+    }
+}
---
+++ UC3_Email/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC3_Email;
+
+namespace UC3_Email
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter the Email: ");
+            string email = Console.ReadLine();
+            bool zip = p.validate_email(email);
+            if (zip)
+            {
+                Console.WriteLine("Valid email");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email");
+            }
+        }
+    }
+}
