43fffb3fcebc6f0e927fbfd93cbeff0b99895175
YogitaDevi <yogita.gry@gmail.com>
2022-12-20 00:21:05 +0530

    [yogita]minimum 8 Character

---
+++ UC5_MinimumCharacter/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC5_MinimumCharacter;
+
+namespace UC5_MinimumCharacter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+            bool pwd = p.validate_pwd(password);
+            if (pwd)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
