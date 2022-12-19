78f85caf47845817c8c99790866051e125fab65e
YogitaDevi <yogita.gry@gmail.com>
2022-12-19 21:54:54 +0530

    Check first name

---
+++ UC1_FName/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UC1_FName
+{
+    internal class Pattern
+    {
+        public static string FName = "^([A-Z]+?)([a-zA-Z]+)*?$";
+        public bool validate_fname(string fname)
+        {
+            return Regex.IsMatch(fname, FName);
+        }
+    }
+}
