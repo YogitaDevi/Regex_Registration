fd5e223b1bc79dbc060a4a104fba04d24b02e9fd
YogitaDevi <yogita.gry@gmail.com>
2022-12-20 00:38:50 +0530

    [yogita]at least 1 numeric number

---
+++ UC7_NumericNumber/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC7_NumericNumber
+{
+    internal class Pattern
+    {
+        public static string Password = @"[0-9]+";
+        public bool validate_pwd(string password)
+        {
+            return Regex.IsMatch(password, Password);
+        }
+    }
+}
---
+++ UC7_NumericNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC7_NumericNumber;
+
+namespace UC7_NumericNumber
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+            bool pwd = p.validate_pwd(password);
+            if (pwd)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
