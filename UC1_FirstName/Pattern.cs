0cfe831c96d2b3c4421c7158fb4c3cda6cd3c27e
YogitaDevi <yogita.gry@gmail.com>
2022-12-19 22:39:03 +0530

    Checking first name

---
+++ UC1_FirstName/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC1_FirstName
+{
+    public class Pattern
+    {
+        public static string FName = "^([A-Z]+?)([a-zA-Z]+)*?$";
+        public bool validate_fname(string fname)
+        {
+            return Regex.IsMatch(fname, FName);
+        }
+    }
+}
---
+++ UC1_FirstName/Program.cs
@@ -0,0 +1,23 @@
+﻿using UC1_FirstName;
+
+namespace UC1_FirstName
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter the First Name: ");
+            string fname = Console.ReadLine();
+            bool name = p.validate_fname(fname);
+            if (name)
+            {
+                Console.WriteLine("Valid first name.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name.");
+            }
+        }
+    }
+}
